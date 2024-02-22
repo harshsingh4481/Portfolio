8ff53ccd81692234a0f14b3ea8c241234da3a942
wanda <vardhanharshsingh9555@gmail.com>
2024-02-23 00:39:53 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var Typed = new Typed(".text", {
+    strings:['Frontend Developer', 'Freelancer' , 'Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
